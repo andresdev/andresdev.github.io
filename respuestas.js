f942cfd1a4c40a3803bde0982fc2e13e8cf3c980
Bataka <andres.uribe1193@gmail.com>
2023-03-10 18:39:19 -0600

    Inicia commit

---
+++ respuestas.js
@@ -0,0 +1,122 @@
+function resultado() {
+    var p1, p2, p3, p4, p5, notA;
+
+    // 1a pregunta
+    if (document.getElementById('p11').checked==true) {p1=1}
+    else {p1=0}
+    
+    // 2a pregunta
+    if (document.getElementById('p21').checked==true) {p2=1}
+    else {p2=0}
+    
+    // 3a pregunta
+    if (document.getElementById('p31').checked==true) {p3=1}
+    else {p3=0}
+    
+    // 4a pregunta
+    if (document.getElementById('p41').checked==true) {p4=1}
+    else {p4=0}
+    
+    // 5a pregunta
+    if (document.getElementById('p51').checked==true) {p5=1}
+    else {p5=0}
+
+    notA=p1+p2+p3+p4+p5;
+    
+    
+    return notA;
+    
+}
+
+function resultadoB(){
+        var b1, b2, b3, b4, b5, notb;
+
+    // 1a pregunta
+    if (document.getElementById('p12').checked==true) {b1=1}
+    else {b1=0}
+    
+    // 2a pregunta
+    if (document.getElementById('p22').checked==true) {b2=1}
+    else {b2=0}
+    
+    // 3a pregunta
+    if (document.getElementById('p32').checked==true) {b3=1}
+    else {b3=0}
+    
+    // 4a pregunta
+    if (document.getElementById('p42').checked==true) {b4=1}
+    else {b4=0}
+    
+    // 5a pregunta
+    if (document.getElementById('p52').checked==true) {b5=1}
+    else {b5=0}
+
+    notb=b1+b2+b3+b4+b5;
+    
+    
+    return notb;
+    
+}
+
+
+
+function resultadoC(){
+        var c1, c2, c3, c4, c5, notc;
+
+    // 1a pregunta
+    if (document.getElementById('p13').checked==true) {c1=1}
+    else {c1=0}
+    
+    // 2a pregunta
+    if (document.getElementById('p23').checked==true) {c2=1}
+    else {c2=0}
+    
+    // 3a pregunta
+    if (document.getElementById('p33').checked==true) {c3=1}
+    else {c3=0}
+    
+    // 4a pregunta
+    if (document.getElementById('p43').checked==true) {c4=1}
+    else {c4=0}
+    
+    // 5a pregunta
+    if (document.getElementById('p53').checked==true) {c5=1}
+    else {c5=0}
+
+    notc=c1+c2+c3+c4+c5;
+    
+    
+    return notc;
+    
+}
+
+
+function resultadoD(){
+        var d1, d2, d3, d4, d5, notd;
+
+    // 1a pregunta
+    if (document.getElementById('p14').checked==true) {d1=1}
+    else {d1=0}
+    
+    // 2a pregunta
+    if (document.getElementById('p24').checked==true) {d2=1}
+    else {d2=0}
+    
+    // 3a pregunta
+    if (document.getElementById('p34').checked==true) {d3=1}
+    else {d3=0}
+    
+    // 4a pregunta
+    if (document.getElementById('p44').checked==true) {d4=1}
+    else {d4=0}
+    
+    // 5a pregunta
+    if (document.getElementById('p54').checked==true) {d5=1}
+    else {d5=0}
+
+    notd=d1+d2+d3+d4+d5;
+    
+    
+    return notd;
+    
+}
